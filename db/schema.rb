055984ab43fd81f3fe844d1efe592c27e0a64455
Amesh Alex Kuruvilla <finixbenny@Ameshs-MacBook-Air.local>
2020-09-24 09:43:21 -0500

    removed shares col from migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_234846) do
+ActiveRecord::Schema.define(version: 2020_09_24_144140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_09_23_234846) do
   create_table "purchases", force: :cascade do |t|
     t.string "coin"
     t.integer "price"
-    t.integer "shares"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
